914b9b2ffad84960b72aa9be40aa187de287a5e4
Dennis Schulmeister-Zimolong <dennis@patk.org>
2023-03-15 16:11:35 +0000

    Grundstruktur der Raspberrypi-Anwendung angelegt

---
+++ Raspberrypi/parkdistance/main.py
@@ -0,0 +1,6 @@
+def main():
+    """
+    Zentraler Einstiegspunkt in das Programm. Erzeugt alle benötigten Objekte
+    und sorgt dafür, dass das Programm im Hintergrund laufen kann.
+    """
+    print("parkdistance sagt Hallo!")
