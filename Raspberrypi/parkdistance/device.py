73bab7f0290b87298c682c206cdf5df3cb4cb298
Dennis Schulmeister-Zimolong <dennis@patk.org>
2023-04-05 04:43:07 +0100

    Vereinfachung der Device-Klasse, wie in der Vorlesung gezeigt

--- Raspberrypi/parkdistance/device.py
+++ Raspberrypi/parkdistance/device.py
@@ -1,76 +1,51 @@
-import logging, time
+import time
 
 class Device:
     """
-    Klasse zur Steuerung des Devoces. Beinhaltet die Hauptschleife des Programms,
-    in der mehrmals je Sekunde die Sensoren abgefragt und die Deviceparameter
-    entsprechend aktualisiert werden. Anhand dieser werden dann die dazugehörigen
-    Aktoren angesteurt.
+    Klasse zur Steuerung des Devices und seines Gesamtzustands. Beinhaltet die
+    Hauptschleife des Programms, in der mehrmals je Sekunde die Sensoren abgefragt
+    und die Deviceparameter entsprechend aktualisiert werden. Anhand dieser werden
+    dann die Aktoren angesteuert.
 
-    Die Device-Parameter finden sich im Dictionary `self.parameters`, das von den
-    Sensoren und Aktoren entsprechend verwendet wird.
+    Die Device-Parameter befinden sich im Dictionary `self.parameters`, das von
+    den Sensoren und Aktoren verwendet wird.
     """
-    
+
     def __init__(self):
         """
         Konstruktor.
         """
-        self._sensors = []
-        self._actors  = []
-
+        self._sensors_actors = []
         self.parameters = {}
+
+    def add_sensor_actor(self, sensor_actor):
+        """
+        Sensor oder Aktor dem Device hinzufügen, damit er in der Hauptschleife
+        des Devices periodisch abgefragt werden kann. Das übergebene Objekt
+        muss ein Callable sein, das als einzigen Parameter ein `Device`-Objekt
+        erwartet.
+        """
+        self._sensors_actors.append(sensor_actor)
     
-    def add_sensor(self, sensor):
+    def loop_forever(self, update_frequency=2):
         """
-        Sensor dem Device hinzufügen. Bei dem übergebenen Sensorbjekt muss es sich
-        um ein Callable handeln (Funktion oder Objekt mit `__call__()`-Methode),
-        dass als einzigen Parameter das Device-Objekt übergeben bekommt.
-
-        Aufgabe der Sensoren ist es, die Deviceparameter zu ändern, anhand derer
-        die Entscheidungen zum Verhalten des Devices getroffen werden. Die Sensoren
-        werden daher immer am *Anfang* der Hauptschleife vor Berechnung der Aktionen
-        angesteuert.
+        Hauptschleife zur Steuerung des Devices. Muss im Hauptprogramm aufgerufen
+        werden, damit das Device periodisch seine Sensoren und Aktorek aufrufen
+        kann und diese ihre Logik ausführen.
         """
-        self._sensors.append(sensor)
-    
-    def add_actor(self, actor):
-        """
-        Aktor dem Device hinzufügen. Bei dem übergebenen Aktorobjekt muss es sich
-        um ein Callable handeln (Funktion oder Objekt mit `__call__()`-Methode),
-        dass als einzigen Parameter das Device-Objekt übergeben bekommt.
-
-        Aufgabe der Aktoren ist es, das auf Basis der Sensorwerte ermittelte Verhalten
-        des Devices auszuführen. Die Aktoren werden daher immer am *Ende* der Hauptschleife
-        nach Berechnung der Aktionen angesteuert.
-        """
-        self._actors.append(actor)
-
-    def loop_forever(self, update_frequency=10):
-        """
-        Hauptschleife zur Steuerung des Devices. Muss aufgerufen werden, damit das
-        Device regelmäßig seine Sensoren prüft und basierend auf den erhaltenen Werten
-        die Parameter ändert und die Aktoren ansteuert.
-
-        Parameter:
-            * update_frequency: Anzahl der Sensorprüfungen pro Sekunde.
-        """
-        target_delay_s = 1.0 / update_frequency
-        prev_time_s = 0
-        needed_delay_s = 0
+        max_sleep_time_s = 1.0 / update_frequency
+        actual_sleep_time_s = 0.0
+        prev_time_s = 0.0
 
         while True:
-            # Thread pausieren, um CPU-Leistung einzusparen
+            # Variable Dauer abwarten, um die gewünschte Update-Frequenz zu erhalten
             current_time_s = time.monotonic()
-            needed_delay_s = target_delay_s - (current_time_s - prev_time_s)
+            actual_sleep_time_s = max_sleep_time_s - (current_time_s - prev_time_s)
             prev_time_s = current_time_s
 
-            if needed_delay_s > 0:
-                time.sleep(needed_delay_s)
-            
-            # Sensoren auslesen
-            for sensor in self._sensors:
-                sensor(self)
-            
-            # Aktoren auslesen
-            for actor in self._actors:
-                actor(self)
+            if actual_sleep_time_s > 0:
+                time.sleep(actual_sleep_time_s)
+
+            # Logik der Sensoren und Aktoren ausführen
+            for sensor_actor in self._sensors_actors:
+                sensor_actor(self)
--- Raspberrypi/parkdistance/main.py
+++ Raspberrypi/parkdistance/main.py
@@ -39,12 +39,38 @@ def main():
     try:
         device = Device()
 
-        device.add_sensor(SilentButton(pin=23, pull_up=True))
-        device.add_sensor(DistanceSensor(trigger_pin=10, echo_pin=9, ringbuffer_size=10))
-
-        device.add_actor(LedBeeper(led1_pin=7, led2_pin=8, buzzer_pin=13))
-        device.add_actor(MQTTHandler(device, config["mqtt"]))
+        device.add_sensor_actor(SilentButton(pin=23, pull_up=True))
+        device.add_sensor_actor(DistanceSensor(trigger_pin=10, echo_pin=9, ringbuffer_size=10))
+        device.add_sensor_actor(LedBeeper(led1_pin=7, led2_pin=8, buzzer_pin=13))
+        device.add_sensor_actor(MQTTHandler(device, config["mqtt"]))
 
         device.loop_forever(update_frequency=1)
     except KeyboardInterrupt:
         pass
+
+    ## Alte Version ohne Entkopplung der Objekte durch die Device-Klasse.
+    ## Für kleinere Anwendungen ist diese Version auch gut bzw. sogar
+    ## etwas einfacher zu verstehen. Je mehr Sensoren und Aktoren jedoch
+    ## hinzu kommen, desto unübersichtlicher wird der Quellcode zu ihrer
+    ## Verknüpfung und desto weniger lässt sich das Offen/Geschlossen-Prinzip
+    ## beim Hinzufügen weiterer Sensoren oder Aktoren einhalten.
+    #
+    # distance_min_m = 0.1
+    # distance_max_m = 1.0
+    #
+    # try:
+    #     while True:
+    #         time.sleep(0.5)
+    #
+    #         distance_m = distance_sensor.measure_distance()
+    #         print(f"Gemessener Abstand: {distance_m}")
+    #
+    #         if silent_button.value == silent_button.PRESSED:
+    #             led_beeper.silent = not led_beeper.silent
+    #             print(f"Ton aus? {led_beeper.silent}")
+    #
+    #         intensity = 1.0 * (distance_m - distance_min_m) / (distance_max_m - distance_min_m)
+    #         intensity = 1 - intensity
+    #         led_beeper.intensity = intensity
+    # except KeyboardInterrupt:
+    #     pass
